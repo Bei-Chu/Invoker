9bf19fa8c598c7c182d48f6e6bb3907dfd98490e
91474 <91474@DESKTOP-12NRPQ6>
2017-01-11 22:46:07 +0800

    detect current ability

--- Invoker/Invoker.py
+++ Invoker/Invoker.py
@@ -1,20 +1,10 @@
-from PIL import ImageGrab
-import cv2
+from PIL import ImageGrab, Image, ImageChops
 import os
 import win32api, win32con
 import pythoncom, pyHook
+import numpy as np
 
 threshold = 100
-
-def read_templates():
-    templates = {}
-    for file in os.listdir(os.curdir):
-        if file.endswith('.png'):
-            img = cv2.imread(file)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            ret, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
-            templates[file[0:-4]] = img
-    return templates
 
 def recognize_letter(region, templates):
     img = ImageGrab.grab(bbox=region)
@@ -23,12 +13,35 @@ def recognize_letter(region, templates):
 
 class AbilityDetector:
     def __init__(self):
+        self.region1 = (927, 955, 939, 968)
+        self.region2 = (985, 955, 997, 968)
         self.ability1 = ''
         self.ability2 = ''
+        self.templates = {}
+        for file in os.listdir(os.curdir):
+            if file.endswith('.png'):
+                img = Image.open(file)
+                self.templates[file[0:-4]] = img
+
+    def match_templates(self, img):
+        errors = {}
+        for name, template in self.templates.items():
+            diff = ImageChops.subtract(template, img)
+            diff = np.array(diff)
+            errors[name] = np.sum(np.abs(diff))
+        return min(errors, key=errors.get)
 
     def detect(self):
-        self.ability1 = ''
-        self.ability2 = ''
+        img = ImageGrab.grab()
+        self.ability1 = self.match_templates(img.crop(self.region1))
+        self.ability2 = self.match_templates(img.crop(self.region2))
+
+    def save_ability_icon(self):
+        img = ImageGrab.grab()
+        icon1 = img.crop(self.region1)
+        icon1.save('icon1.png')
+        icon2 = img.crop(self.region2)
+        icon2.save('icon2.png')
 
 
 class Hook:
@@ -52,7 +65,11 @@ class Hook:
             if (self.detector.ability1 != key and self.detector.ability2 != key):
                 self.trigger_keys(self.short_cuts[key])
                 return False
-        
+
+        if self.vkey.has_key(key):
+            self.trigger_keys(key)
+            return False        
+
         return True
             
     def trigger_keys(self, keys):
